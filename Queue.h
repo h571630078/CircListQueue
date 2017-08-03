44f19bb8706f0065a12831a9dd82f6ff75c30677
Sean <seanlcarstensen@gmail.com>
2017-08-02 21:35:49 -0700

    first implementation of constr and destr

--- Queue.h
+++ Queue.h
@@ -5,21 +5,39 @@ struct Node
 {
     int data;
     Node * next;
+
+    Node(int n)
+    : data(n), next (nullptr)
+    {}
 };
 
 class Queue
 {
   public:
-    Queue();
-    Queue(int cap);
-    ~Queue();
+    Queue()
+    :head (nullptr)
+    {}
+    ~Queue()
+    {
+        if (head != nullptr)
+	{
+	    Node * trav = head->next;
+	    Node * tmp = nullptr;
+	    head->next = nullptr;
+	    while( trav != nullptr )
+	    {
+		tmp = trav;
+	        trav = trav->next;
+		delete tmp;
+	    }
+	}
+    }
     void enqueue(int el);
     int dequeue();
     int peek();
     bool isEmpty();
-    bool isFull();
     void clear();
   private:
-    int * head;
+    Node * head;
     int numNodes;
 };
--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 
 using namespace std;
 
+void test_constr_destr()
+{
+    Queue myQ;
+}
+
 int main()
 {
+    test_constr_destr();
     return 0;
 }
